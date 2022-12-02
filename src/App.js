97b0d2ab792f094ce35f255ef52c5318f829c98c
MDWebDevelopment <md.web.development404@gmail.com>
2022-12-02 13:42:24 +0100

    Final commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ class ActualTemperature extends React.Component {
   onClickChange = () => {
     const value = document.querySelector(".searchBar").value;
     console.log(value);
+    if (value === "") {
+      alert("Nie podano miasta");
+    }
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=e099e7dd0be9664e07a2c9b917e48e73&units=metric`
     )
@@ -19,7 +22,6 @@ class ActualTemperature extends React.Component {
           this.setState({
             city: data.name,
             temperature: data.main.temp,
-            sky: data.weather.description,
           }),
         console.log(this.data)
       )
@@ -27,15 +29,14 @@ class ActualTemperature extends React.Component {
   };
   render() {
     return (
-      <div>
+      <div className="search">
         <input type="text" className="searchBar" placeholder="City"></input>
-        <button className="searchButton" onClick={this.onClickChange}>
+        <button className="button-6" onClick={this.onClickChange}>
           Search
         </button>
         <div className="result">
           <p>City: {this.state.city}</p>
           <p>Temperature: {this.state.temperature}</p>
-          <p>Sky: {this.state.sky}</p>
         </div>
       </div>
     );
